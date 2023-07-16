4a6a735769eeaa438ca8e595a2f709154a68fd57
Abdullah <A21alharbi9@gmail.com>
2023-07-16 08:55:37 +0300

    Add Hero comp shapes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,16 @@
 function Hero() {
   return (
-    <div className="flex justify-center bg-blue-700 h-[40rem] rounded-br-[12rem]">
+    <div className="relative flex justify-center bg-blue-700 h-[40rem] rounded-br-[12rem] overflow-x-hidden">
+      <img
+        src="../assets/shape_group_two.svg"
+        alt="shape_group_two"
+        className="absolute -rotate-45 left-9 bottom-16"
+      />
+      <img
+        src="../assets/shape_group_one.svg"
+        alt="shape_group_one"
+        className="absolute -right-40 transform scale-x-[-1] -rotate-45 top-28"
+      />
       <div className="flex flex-col items-center justify-around text-white">
         <div className="flex flex-col items-center justify-around text-center h-96">
           <h1 className="text-6xl w-[35rem]">UI/UX and Product Designer</h1>
