50919a1670662d39f29ae9ea4b3525e03f89e944
Abdullah <A21alharbi9@gmail.com>
2023-07-18 07:58:43 +0300

    Fix JobInfo comp margins

--- src/app/components/JobInfo.tsx
+++ src/app/components/JobInfo.tsx
@@ -9,7 +9,7 @@ type jobProps = {
 
 function Jobinfo({ type, title, desc, date }: jobProps) {
   return (
-    <div className="flex flex-col items-center justify-around rounded-t-[3rem] rounded-br-[3rem] h-[22rem] lg:max-h-[22rem] w-[95%] lg:w-[45%] xl:w-[45%] group bg-slate-50 hover:scale-105 transition duration-300 cursor-pointer mx-3 lg:mx-0 lg:mb-[4rem]">
+    <div className="flex flex-col items-center justify-around rounded-t-[3rem] rounded-br-[3rem] h-[22rem] lg:max-h-[22rem] w-[95%] lg:w-[45%] xl:w-[45%] group bg-slate-50 hover:scale-105 transition duration-300 cursor-pointer mx-3 my-4 lg:my-0 lg:mx-0 lg:mb-[4rem]">
       <div className="flex flex-col justify-between w-[82%]">
         <p className="text-lg font-semibold text-blue-600">{type}</p>
         <p className="my-3 text-3xl font-bold transition duration-200 group-hover:text-green-500">
